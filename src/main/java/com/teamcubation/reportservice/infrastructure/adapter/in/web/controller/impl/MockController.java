dc358f92182f3e401f79b4c9c00e1c72e4795468
TEC - Heredia Gustavo Michel <gustavo.mph@gmail.com>
2024-10-21 17:23:20 -0300

    Agregado un mock controller para probar las credenciales. Se hardcodeo el username y password para que sean siempre los mismos

--- src/main/java/com/teamcubation/reportservice/infrastructure/adapter/in/web/controller/impl/MockController.java
+++ src/main/java/com/teamcubation/reportservice/infrastructure/adapter/in/web/controller/impl/MockController.java
@@ -1,5 +1,8 @@
 package com.teamcubation.reportservice.infrastructure.adapter.in.web.controller.impl;
 
+import com.teamcubation.reportservice.domain.model.user.UserAuthenticated;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,8 @@ public class MockController {
 
     @GetMapping("/auth")
     public String authenticated() {
-        return "error";
+
+        UserAuthenticated userAthenticated = (UserAuthenticated) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return "Bienvenido " + userAthenticated.getUsername();
     }
 }
--- src/main/java/com/teamcubation/reportservice/infrastructure/adapter/out/persistance/entity/user/MockUserEntity.java
+++ src/main/java/com/teamcubation/reportservice/infrastructure/adapter/out/persistance/entity/user/MockUserEntity.java
@@ -3,13 +3,13 @@ package com.teamcubation.reportservice.infrastructure.adapter.out.persistance.en
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Null;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class MockUserEntity {
