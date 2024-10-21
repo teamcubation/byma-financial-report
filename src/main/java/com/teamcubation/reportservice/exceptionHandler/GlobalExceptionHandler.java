a94c1e4f79e10a57558ace56ceb5ac0aa8e1e363
TEC - Heredia Gustavo Michel <gustavo.mph@gmail.com>
2024-10-21 16:35:24 -0300

    Agregado el Global Exception Handler

---
+++ src/main/java/com/teamcubation/reportservice/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.teamcubation.reportservice.exceptionHandler;
+
+import com.teamcubation.reportservice.domain.MockCustomException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MockCustomException.class)
+    public ResponseEntity<?> handleDuplicatedStockException(MockCustomException e, HttpServletRequest request) {
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(this.createErrorMessage(e, request, HttpStatus.CONFLICT));
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception e, HttpServletRequest request) {
+
+        return ResponseEntity
+                .internalServerError()
+                .body(this.createErrorMessage(e, request, HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    private ErrorMessage createErrorMessage(Exception e, HttpServletRequest request, HttpStatus status) {
+        return ErrorMessage.builder()
+                .message(e.getMessage())
+                .status(status.value())
+                .path(request.getRequestURI())
+                .method(request.getMethod())
+                .build();
+    }
+
+    private static Map<String, String> getValidationErrorsMap(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            FieldError fieldError = (FieldError) error;
+            String fieldName = fieldError.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+}
