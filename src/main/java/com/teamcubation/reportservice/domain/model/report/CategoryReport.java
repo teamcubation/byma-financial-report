15726e2ba2fabd75fd0993fa670857ca498b6bd3
Heber Tescione <hebertescione@hotmail.com>
2024-10-21 16:10:16 -0300

    creo modelo en la capa de dominio y su enum con sus respectivos roles

--- src/main/java/com/teamcubation/reportservice/domain/model/report/CategoryReport.java
+++ src/main/java/com/teamcubation/reportservice/domain/model/report/CategoryReport.java
@@ -1,4 +1,4 @@
-package com.teamcubation.reportservice.domain.model;
+package com.teamcubation.reportservice.domain.model.report;
 
 public enum CategoryReport {
     NAME, PRICE_MIN, PRICE_MAX, CREATION_DATE, TYPE, INTERES_RATE, DIVIDEND
