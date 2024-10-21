eb820b8e8b0309bcc1cb9251665be06a5bdeb377
TEC - Heredia Gustavo Michel <gustavo.mph@gmail.com>
2024-10-21 16:40:02 -0300

    fix en el nombre del UserDetailsServiceImpl

--- src/main/java/com/teamcubation/reportservice/application/service/UserDetailsServiceImpl.java
+++ src/main/java/com/teamcubation/reportservice/application/service/UserDetailsServiceImpl.java
@@ -5,12 +5,13 @@ import com.teamcubation.reportservice.infrastructure.adapter.out.persistance.ent
 import com.teamcubation.reportservice.infrastructure.adapter.out.persistance.repository.user.MockUserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final MockUserRepository userRepository;
 
